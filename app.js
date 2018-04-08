e472f53a5c9436085e47fb579065ac8a4ae909f3
Tim Acker <tjacker31@gmail.com>
2018-04-08 16:15:38 -0400

    Play Until Input
    Added an input field to allow users to set the winning score between 50 and 200 with 100 as the default. Also, added a red shadow around a roll that results in a lost turn.

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ GAME RULES:
 - If the player rolls a 1, all their ROUND score gets lost. After that, it's the next player's turn
 - If the player rolls two 6's in a row during their current turn, their ENTIRE score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that their ROUND score gets added to their GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on ENTIRE score wins the game
+- In the input field, set the TOTAL needed to win between 50 and 200 with a default of 100
+- The first player to reach the set TOTAL on ENTIRE score wins the game
 
 */
 var scores, roundScore, activePlayer, gamePlaying, lastDice;
@@ -27,14 +28,16 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 		if (dice === 6 && lastDice === 6) {
 			scores[activePlayer] = 0;
 			document.querySelector('#score-' + activePlayer).textContent = 0;
+			document.querySelector('.dice').classList.add('lose-turn');
 			nextPlayer();
-		}
-		// Update the round score if the roll is not 1
-		if (dice > 1) {
+		} else if (dice > 1) {
+			// Update the round score if the roll is not 1
 			// Add score
+			document.querySelector('.dice').classList.remove('lose-turn');
 			roundScore += dice;
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
 		} else {
+			document.querySelector('.dice').classList.add('lose-turn');
 			nextPlayer();
 		}
 		lastDice = dice;
@@ -48,9 +51,12 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 	
 		// Update the UI
 		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+		// Get play until value from input field
+		var input = document.getElementById('final-score').value;
 	
 		// Check if player won the game
-		if (scores[activePlayer] >= 100) {
+		if (scores[activePlayer] >= input) {
 			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
 			document.querySelector('.dice').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -89,7 +95,18 @@ function init() {
 
 	document.querySelector('.player-0-panel').classList.remove('winner');
 	document.querySelector('.player-1-panel').classList.remove('winner');
+
+	document.querySelector('.dice').classList.remove('lose-turn');
 }
+
+document.getElementById('final-score').addEventListener('change', function () {
+	// Prevent input from being less than 50 or greater than 200
+	if (!this.value || this.value < 50) {
+		this.value = 50;
+	} else if (this.value > 200) {
+		this.value = 200;
+	}
+})
 
 function nextPlayer() {
 	// Next player
@@ -97,11 +114,10 @@ function nextPlayer() {
 	roundScore = 0;
 	lastDice = 0;
 
+	// Reset score for current round
 	document.getElementById('current-0').textContent = '0';
 	document.getElementById('current-1').textContent = '0';
 	// Toggle active player class
 	document.querySelector('.player-0-panel').classList.toggle('active');
 	document.querySelector('.player-1-panel').classList.toggle('active');
-	// Hide dice
-	document.querySelector('.dice').style.display = 'none';
 }
