53030b7036a08a408f2e0ed59ac642d4cd756d8d
Tim Acker <tjacker31@gmail.com>
2018-04-11 23:09:45 -0400

    ES6
    Refactored code using ES6.

--- app.js
+++ app.js
@@ -10,67 +10,78 @@ GAME RULES:
 - The first player to reach the set TOTAL on their ENTIRE score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying, dice1, dice2;
+let scores, roundScore, activePlayer, gamePlaying
 // Bind functions to shorten name
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
-var $Id = document.getElementById.bind(document);
-// Initialize game
-init();
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const $Id = document.getElementById.bind(document);
 
-function rndNum() {
-	return Math.floor(Math.random() * 6) + 1;
-}
+const init = () => {
+	// Reset game conditions
+	scores = [0, 0];
+	activePlayer = 0;
+	roundScore = 0;
+	gamePlaying = true;
 
-function loseTurn(status) {
-	$$('.dice').forEach(function (die) {
-		// Add / remove class
-		if (status === 'add') {
-			die.classList.add('lose-turn');
-		} else {
-			die.classList.remove('lose-turn');
-		}
+	$$('.dice').forEach((die, i) => die.style.display = 'none');
+	loseTurn('remove');
+
+	scores.forEach((player, i) => {
+		$Id(`score-${i}`).textContent = '0';
+		$Id(`current-${i}`).textContent = '0';
+		$Id(`name-${i}`).textContent = `PLayer ${i + 1}`;
+
+		$(`.player-${i}-panel`).classList.remove('winner');
 	});
+
+	$('.player-0-panel').classList.add('active');
+	$('.player-1-panel').classList.remove('active');
 }
 
-function nextPlayer() {
+// Return dice value
+const getDiceRoll = () => Math.floor(Math.random() * 6) + 1;
+
+// Set lose turn set
+const loseTurn = status => $$('.dice').forEach(die => status === 'add' ? die.classList.add('lose-turn') : die.classList.remove('lose-turn'));
+
+const nextPlayer = () => {
 	// Next player
 	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 	roundScore = 0;
 
-	scores.forEach(function (player, i) {
+	scores.forEach((player, i) => {
 		// Reset score for current round
-		$Id('current-' + i).textContent = '0';
+		$Id(`current-${i}`).textContent = '0';
 		// Toggle active player class
-		$('.player-' + i + '-panel').classList.toggle('active');
+		$(`.player-${i}-panel`).classList.toggle('active');
 	});
 }
  
-$('.btn-roll').addEventListener('click', function () {
+$('.btn-roll').addEventListener('click', () => {
 	if (gamePlaying) {
-		// Get a dice roll
-		dice1 = rndNum();
-		dice2 = rndNum();
+		// Get a dice value
+		let dice = [];
+		dice[0] = getDiceRoll();
+		dice[1] = getDiceRoll();
 
-		$$('.dice').forEach(function (die, i) {
+		$$('.dice').forEach((die, i) => {
 			// Display the result
-			++i;
 			die.style.display = 'block';
-			die.src = 'img/dice-' + window['dice' + i] + '.png';
+			die.src = `img/dice-${dice[i]}.png`;
 		});
 		// Clear entire score if player rolls two 6's in a row during a turn
-		if (dice1 === 6 && dice2 === 6) {
+		if (dice[0] === 6 && dice[1] === 6) {
 			scores[activePlayer] = 0;
-			$('#score-' + activePlayer).textContent = 0;
+			$(`#score-${activePlayer}`).textContent = 0;
 			// Add lose turn class
 			loseTurn('add');
 			nextPlayer();
-		} else if (dice1 > 1 && dice2 > 1) {
+		} else if (dice[0] > 1 && dice[1] > 1) {
 			// Remove lose turn class
 			loseTurn('remove');
 			// Add score
-			roundScore += dice1 + dice2;
-			$('#current-' + activePlayer).textContent = roundScore;
+			roundScore += dice[0] + dice[1];
+			$(`#current-${activePlayer}`).textContent = roundScore;
 		} else {
 			// Add lose turn class
 			loseTurn('add');
@@ -79,23 +90,21 @@ $('.btn-roll').addEventListener('click', function () {
 	}
 });
 
-$('.btn-hold').addEventListener('click', function () {
+$('.btn-hold').addEventListener('click', () => {
 	if (gamePlaying) {
 		// Add current score to global score
 		scores[activePlayer] += roundScore;
 		// Update the UI
-		$('#score-' + activePlayer).textContent = scores[activePlayer];
+		$(`#score-${activePlayer}`).textContent = scores[activePlayer];
 		// Get play until value from input field
 		var input = $Id('final-score').value;
 		// Check if player won the game
 		if (scores[activePlayer] >= input) {
-			$('#name-' + activePlayer).textContent = 'WINNER!';
-			$$('.dice').forEach(function (die, i) {
-				die.style.display = 'none';
-			});
+			$(`#name-${activePlayer}`).textContent = 'WINNER!';
+			$$('.dice').forEach((die, i) => die.style.display = 'none');
 
-			$('.player-' + activePlayer + '-panel').classList.add('winner');
-			$('.player-' + activePlayer + '-panel').classList.remove('active');
+			$(`.player-${activePlayer}-panel`).classList.add('winner');
+			$(`.player-${activePlayer}-panel`).classList.remove('active');
 			gamePlaying = false;
 		} else {
 			// Next player
@@ -104,34 +113,7 @@ $('.btn-hold').addEventListener('click', function () {
 	}
 });
 
-$('.btn-new').addEventListener('click', init);
-
-function init() {
-	// Reset game conditions
-	scores = [0, 0];
-	activePlayer = 0;
-	roundScore = 0;
-	gamePlaying = true;
-
-	$$('.dice').forEach(function (die, i) {
-		++i;
-		die.style.display = 'none';
-	});
-	loseTurn('remove');
-
-	scores.forEach(function (player, i) {
-		$Id('score-' + i).textContent = '0';
-		$Id('current-' + i).textContent = '0';
-		$Id('name-' + i).textContent = 'Player' + (i + 1);
-
-		$('.player-' + i + '-panel').classList.remove('winner');
-	});
-
-	$('.player-0-panel').classList.add('active');
-	$('.player-1-panel').classList.remove('active');
-}
-
-$Id('final-score').addEventListener('change', function () {
+$Id('final-score').addEventListener('change', () => {
 	// Prevent input from being less than 50 or greater than 200
 	if (!this.value || this.value < 50) {
 		this.value = 50;
@@ -139,3 +121,8 @@ $Id('final-score').addEventListener('change', function () {
 		this.value = 200;
 	}
 });
+
+$('.btn-new').addEventListener('click', init);
+
+// Initialize game
+init();
